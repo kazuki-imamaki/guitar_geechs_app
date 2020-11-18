3cd9b854ce6a00f5ba89a015c49ada479c2349e0
kazuki-imamaki <elm.fb.gradual@gmail.com>
2020-11-18 02:51:25 +0000

    Add password reset

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@example.com"
+  default from: "sandbox93b38465e4e74d72b0d6a20b7e05659c.mailgun.org"
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,7 +116,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'powerful-spire-23117.herokuapp.com'
+  host = 'https://powerful-spire-23117.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
@@ -126,4 +126,5 @@ Rails.application.configure do
     :domain         => host,
     :authentication => :plain,
   }
+  
 end
