1f61ec6a62074c48941c3f058c013610f264a44d
kazuki-imamaki <elm.fb.gradual@gmail.com>
2020-11-25 12:52:21 +0000

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
+  belongs_to       :user
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
@@ -8,9 +8,9 @@ class Micropost < ApplicationRecord
                                       message: "must be a valid image format" },
                       size:         { less_than: 5.megabytes,
                                       message: "should be less than 5MB" }
-                                      
+
+  # 表示用のリサイズ済み画像を返す
   def display_image
     image.variant(resize_to_limit: [500, 500])
-  end                                    
+  end
 end
-
