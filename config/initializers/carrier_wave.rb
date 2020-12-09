d991b4ed03f7abb2bd4f87594942c7d496a87b4e
kazuki-imamaki <elm.fb.gradual@gmail.com>
2020-12-09 10:39:25 +0000

    s3 settings for video upload

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,17 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      
-      :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],     
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET']
-  end
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory  = 'imamaki-app'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+    region: ENV['AWS_REGION'],
+    path_style: true
+  }
+
 end
