e67e370bb3f5a7f12c1a6fc22cfafc7e043e661c
kazuki-imamaki <elm.fb.gradual@gmail.com>
2020-12-09 11:22:35 +0000

    s3 env

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['AWS_REGION'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['AWS_SECRET_KEY']
+      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['AWS_BUCKET']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
