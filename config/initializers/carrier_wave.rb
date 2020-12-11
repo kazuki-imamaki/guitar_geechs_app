aa7a971e2f0c17aafec77d6c44284bbe1203b648
kazuki-imamaki <elm.fb.gradual@gmail.com>
2020-12-11 14:43:51 +0000

    S3 aws fog public = false

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,24 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production? || Rails.env.development? # 開発中もs3使う
+    config.storage :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'imamaki-app'
+    config.asset_host = 'https://s3.amazonaws.com/imamaki-app'
+    # NOTE: AWS側の設定を変えなくても、この１行の設定でアップロードできた
+    config.fog_public = false # ←コレ
     config.fog_credentials = {
       provider: 'AWS',
-      region: 'ap-northeast-1', # 問題箇所
       aws_access_key_id: 'AKIAWHJRJMVZ7QEBZUYT',
-      aws_secret_access_key: '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'
+      aws_secret_access_key: 'HAOkXUDzh5t6E2skl1vnM5LoYz8aGXhjSezZ3xz6',
+      region: 'us-east-2',
+      # path_style: true
     }
-    config.fog_directory = 'imamaki-video'
-    config.cache_storage = :fog
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
