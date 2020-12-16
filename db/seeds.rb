86b342546cde401bafef79d6d0cd3eb3f5f8a578
kazuki-imamaki <elm.fb.gradual@gmail.com>
2020-12-16 12:56:47 +0000

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ User.create!(name:  "Kazuki Imamaki",
              activated: true,
              activated_at: Time.zone.now)
 
-99.times do |n|
+5.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -21,17 +21,3 @@ end
 
 
 
-# マイクロポスト
-users = User.order(:created_at).take(25)
-50.times do
-  content = Faker::Lorem.sentence(word_count: 5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
-
-# 以下のリレーションシップを作成する
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
